ad9053d024defc7a071eec2b5db3e6480bc0b891
Ghost <ghsthack@gmail.com>
2014-03-30 01:02:38 +0100

    Install script for FODE.

---
+++ scripts/install-fode.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+using Ionic.Zip;
+using PlayFO;
+using PlayFO.Scripts;
+
+public class Script : IInstallScript
+{
+	public bool Install(string tempDir, string installDir)
+	{
+		String filename = tempDir + Path.DirectorySeparatorChar + "FOUpdater.zip";
+		PlayFO.frmDownload download = new PlayFO.frmDownload("FODE", "http://fode.eu/files/download/2-fonline-desert-europe-game-client/", filename);
+		if (!download.IsDisposed)
+			download.ShowDialog();
+
+		if (!File.Exists(filename))
+		{
+			MessageBox.Show(filename + " not found after download!");
+			return false;
+		}
+
+		ZipFile zip = ZipFile.Read(filename);
+		zip.ExtractSelectedEntries("*.*", "", installDir);
+
+		Process proc = Process.Start(installDir + "\\" + "FOUpdater.exe");
+		while (!proc.HasExited)
+		{
+			if (GetWindowText.WindowContainsTextString("FOUpdater v0.2.1", "Update not needed") ||
+				GetWindowText.WindowContainsTextString("FOUpdater v0.2.1", "Updated ")
+				)
+				proc.Kill();
+		}
+		return true;
+	}
+}
